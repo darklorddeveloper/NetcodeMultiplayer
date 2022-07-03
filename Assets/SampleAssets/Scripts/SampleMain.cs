cdebfbde4e02a92c942582e5b8c57711bd4013d7
darklorddeveloper <darklorddeveloper@gmail.com>
2022-07-03 10:50:18 +0900

    Added host and client sample

---
+++ Assets/SampleAssets/Scripts/SampleMain.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+namespace Sample
+{
+    public class SampleMain : MonoBehaviour
+    {
+        [SerializeField] private RoomUI roomUI;
+
+        public void Start()
+        {
+            SetupUI();
+        }
+
+        private void SetupUI()
+        {
+            roomUI.hostButton.onClick.AddListener(OnClickedHost);
+            roomUI.clientButton.onClick.AddListener(OnClickedClient);
+        }
+
+        private void OnClickedHost()
+        {
+            if(NetworkManager.Singleton.StartHost() == false)
+            {
+                //Failed
+                return;
+            }
+            CloseRoomUI();
+        }
+
+        private void OnClickedClient()
+        {
+            if(NetworkManager.Singleton.StartClient() == false)
+            {
+                //Failed
+                return;
+            }
+            CloseRoomUI();
+        }
+
+        private void CloseRoomUI()
+        {
+            roomUI.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/SampleAssets/Scripts/UI/RoomUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Sample
+{
+    public class RoomUI : MonoBehaviour
+    {
+        public Button hostButton;
+        public Button clientButton;
+    }
+}
